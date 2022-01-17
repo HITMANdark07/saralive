a1f922035e8ed13eb8822d3865d347e9f5dbdd79
Shekhar kumar <shekharkumar235838@gmail.com>
2022-01-18 00:21:09 +0530

    geting performer list

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text, StyleSheet,TouchableOpacity, ScrollView, RefreshControl } from 'react-native';
+import { getDatabase, push, ref, set, orderByChild,remove, equalTo,onChildAdded, query, orderByValue, onValue, update } from "firebase/database";
 import { connect } from 'react-redux';
 import { API } from '../../api.config';
 import Icon from 'react-native-vector-icons/Entypo';
 import PerformerCard from '../components/PerformerCard';
 import Footer from '../components/Footer';
+import axios from 'axios';
 
 const dark= '#10152F';
 const wait = (timeout) => {
@@ -13,15 +15,50 @@ const wait = (timeout) => {
 const Home = ({navigation, currentUser}) => {
 
     const [refreshing, setRefreshing] = React.useState(false);
+    const [performers, setPerformers] = React.useState([]);
+    const [list, setList] = React.useState([]);
 
+    const getPerformersDetails = (listIds) => {
+        const list = [];
+        listIds.forEach((id) => {
+            axios({
+                method:'POST',
+                url:`https://bengalmarine.in/chat_app/api/v1/performer_details`,
+                data:{ user_id : +id}
+            }).then((res) => {
+                if(res.data.responseCode){
+                    list.push(res.data.responseData);
+                    setPerformers((prevState) => [...prevState, res.data.responseData]);
+                }
+            }).catch((err) => {
+                console.log(err);
+            })
+        });
+        // setPerformers(list);
+    }
     const onRefresh = React.useCallback(() => {
         setRefreshing(true);
         wait(2000).then(() => setRefreshing(false));
       }, []);
+    
+    React.useEffect(() => {
+        const db = getDatabase();
+        const paidRef = query(ref(db, 'paidcam'),orderByChild("person2"), equalTo(""));
+        onValue(paidRef, (snapshot) => {
+            const perform = [];
+            snapshot.forEach((snap) => {
+                perform.push(snap.key);
+                // console.log(snap.key);
+            })
+            console.log(perform);
+            getPerformersDetails(perform);
+        })
+    },[]);
+    console.log(performers);
     return (
         <View style={{flex:1, backgroundColor:dark}}>
             <View style={{backgroundColor:'#1A224B', borderBottomLeftRadius:50, borderBottomRightRadius:50, marginBottom:20}}>
-            <Text style={{color:'#fff', fontWeight:'700', alignSelf:'center', fontSize:20,margin:20}}>ONLINE PERFORMERS</Text>
+            <Text style={{color:'#fff', fontWeight:'400', alignSelf:'center', fontSize:20,margin:20}}>ONLINE PERFORMERS</Text>
             </View>
             <ScrollView  refreshControl={
             <RefreshControl
@@ -32,8 +69,19 @@ const Home = ({navigation, currentUser}) => {
             >
             <View style={{flexDirection:'row', justifyContent:'center', marginBottom:100}}>
                 <View style={{flexDirection:'column', justifyContent:'flex-start'}}>
+                {
+                    performers.map((p,idx) => (
+                        <TouchableOpacity activeOpacity={0.5} key={idx} onPress={() => { navigation.navigate('Performer')}}>
+                            <PerformerCard  performer={p} />
+                            {/* <PerformerCard h={idx===0 ? true:false} performer={p} /> */}
+                        </TouchableOpacity>
+                    ))
+                }
+
+                 </View>
+                {/*<View style={{flexDirection:'column'}}>
                 <TouchableOpacity activeOpacity={0.5} onPress={() => { navigation.navigate('Performer')}}>
-                    <PerformerCard h={true} />
+                    <PerformerCard />
                 </TouchableOpacity>
 
                 <TouchableOpacity activeOpacity={0.5} onPress={() => { navigation.navigate('Performer')}}>
@@ -63,40 +111,7 @@ const Home = ({navigation, currentUser}) => {
                 <TouchableOpacity activeOpacity={0.5} onPress={() => { navigation.navigate('Performer')}}>
                     <PerformerCard />
                 </TouchableOpacity>
-                </View>
-                <View style={{flexDirection:'column'}}>
-                <TouchableOpacity activeOpacity={0.5} onPress={() => { navigation.navigate('Performer')}}>
-                    <PerformerCard />
-                </TouchableOpacity>
-
-                <TouchableOpacity activeOpacity={0.5} onPress={() => { navigation.navigate('Performer')}}>
-                    <PerformerCard />
-                </TouchableOpacity>
-
-                <TouchableOpacity activeOpacity={0.5} onPress={() => { navigation.navigate('Performer')}}>
-                    <PerformerCard />
-                </TouchableOpacity>
-
-                <TouchableOpacity activeOpacity={0.5} onPress={() => { navigation.navigate('Performer')}}>
-                    <PerformerCard />
-                </TouchableOpacity>
-
-                <TouchableOpacity activeOpacity={0.5} onPress={() => { navigation.navigate('Performer')}}>
-                    <PerformerCard />
-                </TouchableOpacity>
-
-                <TouchableOpacity activeOpacity={0.5} onPress={() => { navigation.navigate('Performer')}}>
-                    <PerformerCard />
-                </TouchableOpacity>
-
-                <TouchableOpacity activeOpacity={0.5} onPress={() => { navigation.navigate('Performer')}}>
-                    <PerformerCard />
-                </TouchableOpacity>
-
-                <TouchableOpacity activeOpacity={0.5} onPress={() => { navigation.navigate('Performer')}}>
-                    <PerformerCard />
-                </TouchableOpacity>
-                </View>
+                </View> */}
             </View>
             </ScrollView>
             <Footer navigation={navigation} name="discover" />
