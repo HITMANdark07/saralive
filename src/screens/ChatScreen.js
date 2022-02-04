2bc8ce4015ce49119867c0ffcde7eebc616c0e05
Shekhar kumar <shekharkumar235838@gmail.com>
2022-02-05 02:29:16 +0530

    message ui done

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -9,9 +9,49 @@ import ChatListItem from '../components/ChatListItem';
 import Footer from '../components/Footer';
 
 const dark= '#10152F';
+
 const ChatScreen = ({navigation, currentUser}) => {
 
+    const [messages, setMessages] = React.useState([{
+        sender:12,
+        receiver:1,
+        message:"Hi"
+    },
+    {
+        sender:12,
+        receiver:1,
+        message:"Hey There"
+    },
+    {
+        sender:12,
+        receiver:1,
+        message:"Are u there???"
+    },
+    {
+        receiver:12,
+        sender:1,
+        message:"Yes here i am"
+    }]);
     const [message, setMessage] = React.useState("");
+    const Message = ({msg}) => {
+        if(msg.sender==currentUser.user_id){
+            return(
+                <View style={{display:'flex', flexDirection:'row-reverse'}}>
+                    <View style={{backgroundColor:'#1A224B',padding:10, margin:2,marginRight:10, borderRadius:20, borderBottomRightRadius:0}}>
+                        <Text style={{color:'#ffffff'}}>{msg.message}</Text>
+                    </View>
+                </View>
+            )
+        }else{
+            return(
+                <View style={{display:'flex', flexDirection:'row'}}>
+                    <View style={{backgroundColor:'#1A224B',padding:10, margin:2,marginLeft:10, borderRadius:20, borderBottomLeftRadius:0}}>
+                        <Text style={{color:'#ffffff'}}>{msg.message} </Text>
+                    </View>
+                </View>
+            )
+        }
+    }
     return (
         <View style={{flex:1,backgroundColor:dark}}>
             
@@ -23,16 +63,18 @@ const ChatScreen = ({navigation, currentUser}) => {
             <Text style={{color:'#fff', fontWeight:'400', alignSelf:'center', fontSize:18,margin:20, marginLeft:5}}>Performer Test</Text>
             </View>
             <ScrollView style={{flex:1}}>
-                <View >
+                <View style={{display:'flex', flexDirection:'column'}} >
                     
-                    
+                    {messages.map((msg,idx) => (
+                        <Message msg={msg} key={idx} />
+                    ))}
                     
                 </View>
             </ScrollView>
             <View style={styles.chatFoot}>
                 <Ico name="message1" size={28} color='#fff' style={{alignSelf:'center'}} />
                 <TextInput placeholderTextColor='#ddd' style={{flex:1, color:'#fff', alignSelf:'center', marginLeft:10}} value={message} autoCapitalize='none'  onChangeText={(e) => setMessage(e)} placeholder="Type Message Here..."  />
-                <TouchableOpacity style={{alignSelf:'center'}} onPress={() => setMessage("")}>
+                <TouchableOpacity style={{alignSelf:'center'}} onPress={() => setMessages((prevState) => [...prevState,{sender:currentUser.user_id, receiver:1,message:message}])}>
                 <Ic name="send" size={28} color='#fff'  />
                 </TouchableOpacity>
             </View>
