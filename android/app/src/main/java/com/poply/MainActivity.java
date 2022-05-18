200c555c2b8b2c52519e4ad2372414542829baa5
HITMANdark07 <shekharkumar235838@gmail.com>
2022-05-19 00:45:50 +0530

    poply package name change poply user

--- android/app/src/main/java/com/poply/MainActivity.java
+++ android/app/src/main/java/com/poply/MainActivity.java
@@ -1,4 +1,4 @@
-package com.saralive.poply;
+package com.poply.user;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
--- android/app/src/main/java/com/poply/MainApplication.java
+++ android/app/src/main/java/com/poply/MainApplication.java
@@ -1,4 +1,4 @@
-package com.saralive.poply;
+package com.poply.user;
 
 import android.app.Application;
 import android.content.Context;
